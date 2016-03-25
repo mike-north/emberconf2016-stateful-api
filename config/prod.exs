f3ee811709156410b78fd030da6d6c20b481c409
Mike North <michael.l.north@gmail.com>
2016-03-25 10:59:39 -0700

    remove static stuff from prod again

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,6 @@ config :pullrequest, Pullrequest.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "emberconf-state-api.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :pullrequest, Pullrequest.Repo,
