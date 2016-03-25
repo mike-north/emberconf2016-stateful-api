aaac264a1070b5db028cdeddd097b8b1cc93d372
Mike North <michael.l.north@gmail.com>
2016-03-25 10:45:14 -0700

    Prod configuration

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,12 +4,10 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :pullrequest, Pullrequest.Endpoint,
-  secret_key_base: "wPXFdD2wiy/dU2s/KPBg4YlW35haBxKDsw8Oua/hb7xz56Xav+gfkTt1z2OudDcb"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :pullrequest, Pullrequest.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "pullrequest_prod",
+  url: System.get_env("DATABASE_URL"),
   pool_size: 20
