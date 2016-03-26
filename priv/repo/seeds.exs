72fbd20ec5a72525442e0f0ea740eb83adab571d
Mike North <michael.l.north@gmail.com>
2016-03-26 12:57:59 -0700

    More objects

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -20,7 +20,7 @@ client = Tentacat.Client.new(%{access_token: System.get_env("GH_TOKEN") || ""})
       icon_url: org_info["avatar_url"]
     }
     Tentacat.Repositories.list_orgs(org_name, client)
-    |> Enum.take_random(3)
+    |> Enum.take_random(15)
     |> Enum.each fn repo_info ->
       repo = Pullrequest.Repo.insert! %Pullrequest.Repository{
         organization_id: project.id,
@@ -29,7 +29,7 @@ client = Tentacat.Client.new(%{access_token: System.get_env("GH_TOKEN") || ""})
         homepage: repo_info["homepage"]
       }
       Tentacat.Pulls.list(org_name, repo_info["name"], client)
-      |> Enum.take_random(5)
+      |> Enum.take_random(25)
       |> Enum.each fn pull_info ->
         pull = Pullrequest.Repo.insert! %Pullrequest.Pull{
           repository_id: repo.id,
