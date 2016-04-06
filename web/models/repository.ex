3a90d21911f3fa7bb712a1fe26588b9811011f0e
Mike North <michael.l.north@gmail.com>
2016-04-06 18:28:09 +0200

    models, stargazers added to model

--- web/models/repository.ex
+++ web/models/repository.ex
@@ -5,6 +5,8 @@ defmodule Pullrequest.Repository do
     field :name, :string
     field :description, :string
     field :homepage, :string
+    field :stargazers_count, :integer
+    field :watchers_count, :integer
     belongs_to :organization, Pullrequest.Organization
     has_many :pulls, Pullrequest.Pull
 
@@ -12,7 +14,7 @@ defmodule Pullrequest.Repository do
   end
 
   @required_fields ~w(name description homepage organization_id)
-  @optional_fields ~w()
+  @optional_fields ~w(stargazers_count watchers_count)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
